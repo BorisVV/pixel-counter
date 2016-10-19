cb3080427ff6a31b0cb30908da2032f79fdc8ce1
BorisV <Boris V@DESKTOP-E650BMS>
2016-10-18 19:57:57 -0500

    GUI design done

--- PixelsInWindow/Form1.cs
+++ PixelsInWindow/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PixelsInWindow
 {
-    public partial class Form1 : Form
+    public partial class PixelsLabel : Form
     {
-        public Form1()
+        public PixelsLabel()
         {
             InitializeComponent();
         }
